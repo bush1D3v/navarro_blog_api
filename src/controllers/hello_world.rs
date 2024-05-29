bd03f14d69086d23082308cc19929be7f82604cb
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-05-29 09:13:11 -0300

    update: Add a utoipa router configurator for swagger

--- src/controllers/hello_world.rs
+++ src/controllers/hello_world.rs
@@ -1,6 +1,25 @@
 use actix_web::{get, HttpResponse, Responder};
+use serde::Serialize;
+use utoipa::{OpenApi, ToSchema};
 
-#[get("/")]
+#[derive(OpenApi)]
+#[openapi(paths(hello_world), components(schemas(Message)))]
+#[derive(Serialize, ToSchema)]
+pub struct Message {
+    message: String,
+}
+
+#[utoipa::path(
+    responses((
+        status = 200, description = "Route responsible for get a hello world message", body = Message,
+        content_type = "application/json", example = json!({"message": "Hello World!"})
+    ))
+)]
+#[get("/world")]
 async fn hello_world() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    let message: Message = Message {
+        message: String::from("Hello World!"),
+    };
+
+    HttpResponse::Ok().json(message)
 }
