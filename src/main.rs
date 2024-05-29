7ee46c841f89768a7718674cea986ae24567d0dc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-05-29 09:08:23 -0300

    update: Add a swagger service and update a bind format

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use std::env;
-
-use actix_web::{http::KeepAlive, App, HttpServer};
-
+mod config;
 mod controllers;
-use controllers::hello_world::hello_world;
+
+use crate::controllers::hello_world::hello_world;
+use actix_web::{http::KeepAlive, App, HttpServer};
+use config::api_doc::api_doc;
+use std::{env, net::Ipv4Addr};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let http_port: String = env::var("HTTP_PORT").unwrap_or("8080".into());
 
-    HttpServer::new(move || App::new().service(hello_world))
+    HttpServer::new(move || App::new().service(hello_world).service(api_doc()))
         .keep_alive(KeepAlive::Os)
-        .bind(format!("0.0.0.0:{http_port}"))?
+        .bind((Ipv4Addr::UNSPECIFIED, http_port.parse().unwrap_or(8080)))?
         .run()
         .await?;
 
