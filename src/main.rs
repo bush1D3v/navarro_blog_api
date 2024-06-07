2a277d00ab009a07b9bfda66f0865eac04e59135
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-07 19:16:54 -0300

    update: Add pool conections(queue, redis and postgres) and cors configuration

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
 mod config;
 mod controllers;
 mod dtos;
+mod exceptions;
+mod middlewares;
+mod repositories;
+mod services;
 
-use crate::controllers::hello_world::hello_world;
-use actix_web::{http::KeepAlive, App, HttpServer};
-use config::api_doc::api_doc;
-use std::{env, net::Ipv4Addr};
+use actix_web::{http::KeepAlive, web, App, HttpServer};
+use config::queue::{db_flush_queue, AppQueue};
+use config::redis::Redis;
+use config::{api_doc::api_doc, cors::cors, postgres::postgres};
+use controllers::user::insert_user;
+use std::{env, net::Ipv4Addr, sync::Arc};
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    env_logger::init();
     let http_port: String = env::var("HTTP_PORT").unwrap_or("8080".into());
 
-    HttpServer::new(move || App::new().service(hello_world).service(api_doc()))
-        .keep_alive(KeepAlive::Os)
-        .bind((Ipv4Addr::UNSPECIFIED, http_port.parse().unwrap_or(8080)))?
-        .run()
-        .await?;
+    let redis_pool = Redis::pool().await;
+    let pool = postgres();
+    let pool_async = pool.clone();
+    let queue = Arc::new(AppQueue::new());
+    let queue_async = queue.clone();
+    tokio::spawn(async move { db_flush_queue(pool_async, queue_async).await });
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(cors())
+            .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(redis_pool.clone()))
+            .app_data(web::Data::new(queue.clone()))
+            .service(api_doc())
+            .service(insert_user)
+    })
+    .keep_alive(KeepAlive::Os)
+    .bind((Ipv4Addr::UNSPECIFIED, http_port.parse().unwrap_or(8080)))?
+    .run()
+    .await?;
 
     Ok(())
 }
