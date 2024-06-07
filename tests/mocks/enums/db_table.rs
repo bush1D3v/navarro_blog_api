ec1abae5104ca9d806e433f7783812c7bcc90e77
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-07 19:15:40 -0300

    test: Create a database tables Enum

---
+++ tests/mocks/enums/db_table.rs
@@ -0,0 +1,11 @@
+pub enum TablesEnum {
+    Users,
+    _Posts,
+    _Categories,
+    _Tags,
+    _Comments,
+    _PostsTags,
+    _PostsCategories,
+    _UsersPostsLikes,
+    _UsersCommentsLikes,
+}
