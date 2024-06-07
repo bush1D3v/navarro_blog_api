827363b5a1c60ac3bc9d27b6ec732558a4cdc985
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-07 19:13:56 -0300

    feat: Create a user entity repository

---
+++ src/repositories/user.rs
@@ -0,0 +1,27 @@
+use crate::{
+    config::queue::AppQueue,
+    dtos::user::{CreateUserDTO, UserDTO},
+};
+use actix_web::web::{Data, Json};
+use std::sync::Arc;
+
+pub async fn insert_user_repository(
+    queue: Data<Arc<AppQueue>>,
+    body: Json<CreateUserDTO>,
+    id: &String,
+) -> UserDTO {
+    let name: String = body.name.clone();
+    let email: String = body.email.clone();
+    let password: String = body.password.clone();
+    let created_at: String = chrono::Utc::now().to_string();
+    let dto: UserDTO = UserDTO {
+        id: id.clone(),
+        name,
+        email,
+        password,
+        created_at: created_at.clone(),
+    };
+    queue.push((id.clone(), body, Some(created_at)));
+
+    dto
+}
