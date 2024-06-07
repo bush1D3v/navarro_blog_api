9dfcc3f4a63f985dcebecd620d632459ec756f95
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-07 19:08:03 -0300

    config: Update a swagger doc

--- src/config/api_doc.rs
+++ src/config/api_doc.rs
@@ -1,15 +1,29 @@
-use crate::controllers::hello_world::{Message, __path_hello_world};
+use crate::{controllers::user::__path_insert_user, dtos::user::CreateUserDTO};
+use serde::{Deserialize, Serialize};
 use utoipa::{
     openapi::security::{HttpAuthScheme, HttpBuilder, SecurityScheme},
-    Modify, OpenApi,
+    openapi::Components,
+    Modify, OpenApi, ToSchema,
 };
 use utoipa_swagger_ui::SwaggerUi;
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+struct ValidationErrors {
+    password: Vec<ErrorDetail>,
+}
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+struct ErrorDetail {
+    code: String,
+    message: String,
+    params: std::collections::HashMap<String, serde_json::Value>,
+}
 
 pub fn api_doc() -> SwaggerUi {
     #[derive(OpenApi)]
     #[openapi(
-        paths(hello_world),
-        components(schemas(Message)),
+        paths(insert_user),
+        components(schemas(CreateUserDTO, ValidationErrors, ErrorDetail)),
         modifiers(&SecurityModifier),
         servers((
             url = "http://localhost:8080",
@@ -18,13 +32,19 @@ pub fn api_doc() -> SwaggerUi {
             url = "https://hub.docker.com/repository/docker/bush1d3v/navarro_blog_api",
             description = "Docker Image",
         )),
+        tags((
+            name = "user", description = "Controladores da entidade de usuário"
+        )),
     )]
     pub struct ApiDoc;
 
     struct SecurityModifier;
     impl Modify for SecurityModifier {
         fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
-            let components: &mut utoipa::openapi::Components = openapi.components.as_mut().unwrap();
+            let components: &mut Components = match openapi.components.as_mut() {
+                Some(components) => components,
+                None => unreachable!(),
+            };
             components.add_security_scheme(
                 "bearer_auth",
                 SecurityScheme::Http(
