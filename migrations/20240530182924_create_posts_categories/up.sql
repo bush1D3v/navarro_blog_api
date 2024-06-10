af91b40dffc0a022d430cbae7eaf5c6c52aac6c8
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-10 19:43:40 -0300

    refactor: Reorder the foreign key values

--- migrations/20240530182924_create_posts_categories/up.sql
+++ migrations/20240530182924_create_posts_categories/up.sql
@@ -2,6 +2,6 @@ CREATE TABLE posts_categories (
     post_id        UUID         NOT NULL,
     category_id    UUID         NOT NULL,
     PRIMARY KEY (post_id, category_id),
-    FOREIGN KEY (post_id) REFERENCES posts(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
+    FOREIGN KEY (post_id)                   REFERENCES posts(id),
+    FOREIGN KEY (category_id)               REFERENCES categories(id)
 );
