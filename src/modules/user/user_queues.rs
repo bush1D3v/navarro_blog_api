ab72dbe5157ad06c5ddca50c1f88fcc8159ff475
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-17 20:41:35 -0300

    update: Adding a salt in the password of the insert user

--- src/modules/user/user_queues.rs
+++ src/modules/user/user_queues.rs
@@ -2,17 +2,23 @@ use super::user_dtos::CreateUserDTO;
 use crate::shared::exceptions::custom_error_to_io_error_kind::{
     custom_error_to_io_error_kind, CustomError,
 };
+use actix_web::web::{Data, Json};
 use deadpool_postgres::Pool;
 use sql_builder::{quote, SqlBuilder};
 use std::{sync::Arc, time::Duration};
 
-type QueueEvent = (String, actix_web::web::Json<CreateUserDTO>, Option<String>);
+type SaltData = (String, String, Data<deadpool_postgres::Pool>);
+type QueueEvent = (String, Json<CreateUserDTO>, Option<String>, SaltData);
 pub type CreateUserAppQueue = deadqueue::unlimited::Queue<QueueEvent>;
 
-async fn user_insert(pool: Pool, queue: Arc<CreateUserAppQueue>) -> Result<(), std::io::Error> {
-    let mut sql = String::new();
+async fn insert_user_queue(
+    pool: Pool,
+    queue: Arc<CreateUserAppQueue>,
+) -> Result<(), std::io::Error> {
+    let mut user_sql = String::new();
+    let mut user_salt_sql = String::new();
     while queue.len() > 0 {
-        let (id, body, created_at) = queue.pop().await;
+        let (id, body, created_at, salt) = queue.pop().await;
         let mut sql_builder = SqlBuilder::insert_into("users");
         sql_builder
             .field("id")
@@ -32,7 +38,17 @@ async fn user_insert(pool: Pool, queue: Arc<CreateUserAppQueue>) -> Result<(), s
             Err(_) => continue,
         };
         this_sql.pop();
-        sql.push_str(this_sql.as_str());
+        user_sql.push_str(this_sql.as_str());
+
+        let mut sql_builder = SqlBuilder::insert_into("salt");
+        sql_builder.field("user_id").field("salt");
+        sql_builder.values(&[&quote(&id), &quote(&salt.0)]);
+        let mut this_sql = match sql_builder.sql() {
+            Ok(x) => x,
+            Err(_) => continue,
+        };
+        this_sql.pop();
+        user_salt_sql.push_str(this_sql.as_str());
     }
     let mut conn = match pool.get().await {
         Ok(x) => x,
@@ -52,7 +68,16 @@ async fn user_insert(pool: Pool, queue: Arc<CreateUserAppQueue>) -> Result<(), s
             ))
         }
     };
-    match transaction.batch_execute(&sql).await {
+    match transaction.batch_execute(&user_sql).await {
+        Ok(_) => (),
+        Err(e) => {
+            return Err(std::io::Error::new(
+                custom_error_to_io_error_kind(CustomError::TokioPostgres(&e)),
+                e,
+            ))
+        }
+    };
+    match transaction.batch_execute(&user_salt_sql).await {
         Ok(_) => (),
         Err(e) => {
             return Err(std::io::Error::new(
@@ -75,11 +100,16 @@ async fn user_insert(pool: Pool, queue: Arc<CreateUserAppQueue>) -> Result<(), s
 
 pub async fn user_flush_queue(pool_async: Pool, queue_async: Arc<CreateUserAppQueue>) {
     loop {
-        tokio::time::sleep(Duration::from_secs_f32(0.5)).await;
+        tokio::time::sleep(Duration::from_secs(2)).await;
         let queue = queue_async.clone();
         if queue.len() == 0 {
             continue;
         }
-        user_insert(pool_async.clone(), queue).await.unwrap();
+        match insert_user_queue(pool_async.clone(), queue).await {
+            Ok(_) => (),
+            Err(e) => {
+                std::io::Error::new(e.kind(), e);
+            }
+        }
     }
 }
