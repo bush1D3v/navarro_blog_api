e233e403b2794fa302a384c9d7dd1da26ca38c15
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-17 20:32:51 -0300

    update: Force reading env values

--- src/config/cors.rs
+++ src/config/cors.rs
@@ -3,10 +3,12 @@ use actix_web::http;
 use std::env;
 
 pub fn cors() -> Cors {
-    let http_port = env::var("HTTP_PORT").unwrap_or("8080".into());
-
     Cors::default()
-        .allowed_origin(&format!("http://localhost:{}", http_port))
+        .allowed_origin(&format!(
+            "{}:{}",
+            env::var("BASE_URL").unwrap(),
+            env::var("HTTP_PORT").unwrap()
+        ))
         .allowed_methods(vec!["GET", "POST", "PATCH", "DELETE"])
         .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
         .allowed_header(http::header::CONTENT_TYPE)
