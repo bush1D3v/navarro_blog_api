6f3980fba630a509f70107bd1f9118cc73c3312e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-22 23:54:55 -0300

    update: Removing unnused CustomError enums

--- src/shared/exceptions/custom_error_to_io_error_kind.rs
+++ src/shared/exceptions/custom_error_to_io_error_kind.rs
@@ -1,17 +1,13 @@
 pub enum CustomError {
     PoolError,
-    Bcrypt,
     TokioPostgres,
-    JWTError,
     AnyhowError,
 }
 
 pub fn custom_error_to_io_error_kind(error: CustomError) -> std::io::ErrorKind {
     match error {
         CustomError::PoolError => std::io::ErrorKind::ConnectionAborted,
-        CustomError::Bcrypt => std::io::ErrorKind::InvalidInput,
         CustomError::TokioPostgres => std::io::ErrorKind::ConnectionAborted,
-        CustomError::JWTError => std::io::ErrorKind::Other,
         CustomError::AnyhowError => std::io::ErrorKind::Other,
     }
 }
