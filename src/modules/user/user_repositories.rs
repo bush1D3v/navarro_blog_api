8c17ac994a5935dc397c00e65cd79331ae25eeec
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-27 00:01:37 -0300

    update: Create a detail user repository

--- src/modules/user/user_repositories.rs
+++ src/modules/user/user_repositories.rs
@@ -9,7 +9,7 @@ use super::{
 use actix_web::web::{Data, Json};
 use serde::Serialize;
 use sql_builder::quote;
-use std::sync::Arc;
+use std::{io::ErrorKind, sync::Arc};
 
 pub async fn get_user_salt_repository(
     user_id: String,
@@ -65,6 +65,12 @@ pub async fn get_user_salt_repository(
         }
     };
 
+    if rows.len() == 0 {
+        return Err(std::io::Error::new(
+            custom_error_to_io_error_kind(CustomError::AnyhowError),
+            "Erro inesperado do servidor, tente novamente mais tarde.",
+        ));
+    }
     let salt: uuid::Uuid = rows[0].get("salt");
     Ok(salt.to_string())
 }
@@ -90,7 +96,7 @@ pub async fn insert_user_repository(
     queue.push((
         user_id.clone(),
         body,
-        Some(created_at),
+        created_at,
         (user_salt, user_id, pool),
     ));
 
@@ -157,9 +163,86 @@ pub async fn login_user_repository(
         }
     };
 
+    if rows.len() == 0 {
+        return Err(std::io::Error::new(
+            ErrorKind::NotFound,
+            "Não foi encontrado um usuário com este e-mail.",
+        ));
+    }
     let id: uuid::Uuid = rows[0].get("id");
     Ok(LoginUserRepositoryResponse {
         id: id.to_string(),
+        password: rows[0].get("password"),
+    })
+}
+
+pub async fn detail_user_repository(
+    pg_pool: Data<deadpool_postgres::Pool>,
+    user_id: String,
+) -> Result<UserDTO, std::io::Error> {
+    let mut sql_builder = sql_builder::SqlBuilder::select_from("users");
+    sql_builder.or_where_eq("id", &quote(&user_id));
+
+    let mut conn = match pg_pool.get().await {
+        Ok(x) => x,
+        Err(e) => {
+            return Err(std::io::Error::new(
+                custom_error_to_io_error_kind(CustomError::PoolError),
+                e,
+            ))
+        }
+    };
+    let transaction = match conn.transaction().await {
+        Ok(x) => x,
+        Err(e) => {
+            return Err(std::io::Error::new(
+                custom_error_to_io_error_kind(CustomError::TokioPostgres),
+                e,
+            ))
+        }
+    };
+    let sql = match sql_builder.sql() {
+        Ok(x) => x,
+        Err(e) => {
+            return Err(std::io::Error::new(
+                custom_error_to_io_error_kind(CustomError::AnyhowError),
+                e,
+            ))
+        }
+    };
+    let rows = match transaction.query(sql.as_str(), &[]).await {
+        Ok(x) => x,
+        Err(e) => {
+            return Err(std::io::Error::new(
+                custom_error_to_io_error_kind(CustomError::TokioPostgres),
+                e,
+            ))
+        }
+    };
+    match transaction.commit().await {
+        Ok(_) => (),
+        Err(e) => {
+            return Err(std::io::Error::new(
+                custom_error_to_io_error_kind(CustomError::TokioPostgres),
+                e,
+            ))
+        }
+    };
+
+    if rows.len() == 0 {
+        return Err(std::io::Error::new(
+            ErrorKind::NotFound,
+            "Não foi encontrado um usuário com este id.",
+        ));
+    }
+
+    let user_id: uuid::Uuid = rows[0].get("id");
+    let created_at: chrono::DateTime<chrono::Utc> = rows[0].get("created_at");
+    Ok(UserDTO {
+        id: user_id.to_string(),
+        name: rows[0].get("name"),
+        email: rows[0].get("email"),
+        created_at: created_at.to_string(),
         password: rows[0].get("password"),
     })
 }
