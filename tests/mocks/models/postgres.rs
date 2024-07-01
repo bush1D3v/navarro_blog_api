c6a59b8be62499636df34afed3e673d2ae44ad48
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-30 22:17:15 -0300

    test: Creating a postgres mock model

--- tests/mocks/models/postgres.rs
+++ tests/mocks/models/postgres.rs
@@ -2,14 +2,30 @@ use deadpool_postgres::{Config, Pool, PoolConfig, Runtime};
 use std::env;
 use tokio_postgres::NoTls;
 
-pub fn postgres_error() -> Pool {
-    let mut cfg = Config::new();
-    cfg.host = Some(env::var("DB_HOST").unwrap());
-    cfg.port = Some(env::var("DB_PORT").unwrap().parse().unwrap());
-    cfg.dbname = Some(env::var("DB_NAME").unwrap());
-    cfg.user = Some(env::var("DB_USER").unwrap());
-    cfg.password = Some(String::from("5555"));
-    cfg.pool = PoolConfig::new(env::var("POOL_SIZE").unwrap().parse::<usize>().unwrap()).into();
+pub struct PostgresModels {}
 
-    cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap()
+impl PostgresModels {
+    pub fn postgres_success() -> Pool {
+        let mut cfg: Config = Config::new();
+        cfg.host = Some(env::var("DB_HOST").unwrap());
+        cfg.port = Some(env::var("DB_PORT").unwrap().parse().unwrap());
+        cfg.dbname = Some(env::var("DB_NAME").unwrap());
+        cfg.user = Some(env::var("DB_USER").unwrap());
+        cfg.password = Some(env::var("DB_PASSWORD").unwrap());
+        cfg.pool = PoolConfig::new(env::var("POOL_SIZE").unwrap().parse::<usize>().unwrap()).into();
+
+        cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap()
+    }
+
+    pub fn postgres_error() -> Pool {
+        let mut cfg: Config = Config::new();
+        cfg.host = Some(env::var("DB_HOST").unwrap());
+        cfg.port = Some(env::var("DB_PORT").unwrap().parse().unwrap());
+        cfg.dbname = Some(env::var("DB_NAME").unwrap());
+        cfg.user = Some(env::var("DB_USER").unwrap());
+        cfg.password = Some(String::from("5555"));
+        cfg.pool = PoolConfig::new(env::var("POOL_SIZE").unwrap().parse::<usize>().unwrap()).into();
+
+        cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap()
+    }
 }
