bd9f506e4a352e3cdefa616437adf2eeea18fd15
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-06-30 22:14:41 -0300

    refactor: Refactoring a postgres struct

--- src/infra/postgres.rs
+++ src/infra/postgres.rs
@@ -2,14 +2,17 @@ use deadpool_postgres::{Config, Pool, PoolConfig, Runtime};
 use std::env;
 use tokio_postgres::NoTls;
 
-pub fn postgres() -> Pool {
-    let mut cfg = Config::new();
-    cfg.host = Some(env::var("DB_HOST").unwrap());
-    cfg.port = Some(env::var("DB_PORT").unwrap().parse().unwrap());
-    cfg.dbname = Some(env::var("DB_NAME").unwrap());
-    cfg.user = Some(env::var("DB_USER").unwrap());
-    cfg.password = Some(env::var("DB_PASSWORD").unwrap());
-    cfg.pool = PoolConfig::new(env::var("POOL_SIZE").unwrap().parse::<usize>().unwrap()).into();
+pub struct Postgres {}
 
-    cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap()
+impl Postgres {
+    pub fn pool() -> Pool {
+        let mut cfg = Config::new();
+        cfg.host = Some(env::var("DB_HOST").unwrap());
+        cfg.port = Some(env::var("DB_PORT").unwrap().parse().unwrap());
+        cfg.dbname = Some(env::var("DB_NAME").unwrap());
+        cfg.user = Some(env::var("DB_USER").unwrap());
+        cfg.password = Some(env::var("DB_PASSWORD").unwrap());
+        cfg.pool = PoolConfig::new(env::var("POOL_SIZE").unwrap().parse::<usize>().unwrap()).into();
+        cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap()
+    }
 }
