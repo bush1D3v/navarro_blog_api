6ee1d718537f3a2525ff44f8d0b4e4f580d637e1
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-09 16:47:03 -0300

    update: Update a return to HttpResponse

--- src/shared/exceptions/custom_error_to_io_error_kind.rs
+++ src/shared/exceptions/custom_error_to_io_error_kind.rs
@@ -1,13 +1,53 @@
+use crate::utils::error_construct::error_construct;
+use actix_web::HttpResponse;
+use deadpool_postgres::PoolError;
+use std::fmt::{Display, Formatter};
+
 pub enum CustomError {
-    PoolError,
-    TokioPostgres,
-    AnyhowError,
+    PoolError(PoolError),
+    TokioPostgres(tokio_postgres::error::Error),
+    AnyhowError(anyhow::Error),
 }
 
-pub fn custom_error_to_io_error_kind(error: CustomError) -> std::io::ErrorKind {
+impl Display for CustomError {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match self {
+            CustomError::PoolError(e) => write!(f, "{}", e),
+            CustomError::TokioPostgres(e) => write!(f, "{}", e),
+            CustomError::AnyhowError(e) => write!(f, "{}", e),
+        }
+    }
+}
+
+pub fn custom_error_to_io_error_kind(error: CustomError) -> HttpResponse {
     match error {
-        CustomError::PoolError => std::io::ErrorKind::ConnectionAborted,
-        CustomError::TokioPostgres => std::io::ErrorKind::ConnectionAborted,
-        CustomError::AnyhowError => std::io::ErrorKind::Other,
+        CustomError::PoolError(error) => HttpResponse::ServiceUnavailable().json(error_construct(
+            String::from("database"),
+            String::from("service unavailable"),
+            error.to_string(),
+            None,
+            None,
+            None,
+        )),
+        CustomError::TokioPostgres(error) => {
+            HttpResponse::ServiceUnavailable().json(error_construct(
+                String::from("database"),
+                String::from("service unavailable"),
+                error.to_string(),
+                None,
+                None,
+                None,
+            ))
+        }
+        CustomError::AnyhowError(error) => {
+            HttpResponse::InternalServerError().json(error_construct(
+                String::from("server"),
+                String::from("internal server error"),
+                error.to_string(),
+                None,
+                None,
+                None,
+            ))
+        }
     }
 }
