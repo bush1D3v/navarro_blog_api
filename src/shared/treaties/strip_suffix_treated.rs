1f57de05b012df429f847ad593b85f0f551405c6
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-09 16:50:46 -0300

    feat: Creating a treated stripsuffix struct

---
+++ src/shared/treaties/strip_suffix_treated.rs
@@ -0,0 +1,22 @@
+use crate::utils::error_construct::error_construct;
+use actix_web::HttpResponse;
+
+pub struct StripSuffix {}
+
+impl StripSuffix {
+    pub fn strip_suffix(input: String, suffix: &str) -> Result<String, HttpResponse> {
+        match input.strip_suffix(suffix) {
+            Some(input_suffixed) => Ok(input_suffixed.to_string()),
+            None => {
+                return Err(HttpResponse::InternalServerError().json(error_construct(
+                    String::from("server"),
+                    String::from("internal server error"),
+                    String::from("Erro ao extrair o salt do usuário."),
+                    None,
+                    None,
+                    None,
+                )))
+            }
+        }
+    }
+}
