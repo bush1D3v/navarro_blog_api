fd1be09b6ee6075616efb9ca689c22c9054b2467
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-28 18:26:28 -0300

    update: Returning token claims data

--- src/middlewares/jwt_token_middleware.rs
+++ src/middlewares/jwt_token_middleware.rs
@@ -5,6 +5,39 @@ use crate::{
 use actix_web::{http::header::HeaderMap, HttpResponse};
 use jsonwebtoken::TokenData;
 
+/// Middleware to check if the JWT token is valid.
+///
+/// # Parameters
+///
+/// - `headers`: The headers of the request.
+///
+/// # Returns
+///
+/// Returns a `Result` which, on success, return the decoded `TokenData`. On failure, returns an `HttpResponse` with the corresponding error.
+///
+/// # Errors
+///
+/// This function may return an error if:
+///
+/// - The authorization header is missing.
+/// - The authorization header is malformed.
+/// - The authorization header is invalid.
+///
+/// # Examples
+///
+/// ```rust
+/// use navarro_blog_api::middlewares::jwt_token_middleware::jwt_token_middleware;
+/// use navarro_blog_api::shared::structs::jwt_claims::Claims;
+/// use actix_web::{HttpRequest, HttpResponse};
+/// use jsonwebtoken::TokenData;
+///
+/// pub fn example(req: HttpRequest) -> Result<TokenData<Claims>, HttpResponse> {
+///     match jwt_token_middleware(req.headers()) {
+///         Ok(token) => Ok(token),
+///         Err(e) => return Err(e),
+///     }
+/// }
+/// ```
 pub fn jwt_token_middleware(headers: &HeaderMap) -> Result<TokenData<Claims>, HttpResponse> {
     let token = match headers.get("Authorization") {
         Some(header_value) => match header_value.to_str() {
