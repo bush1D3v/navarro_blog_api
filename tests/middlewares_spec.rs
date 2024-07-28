cdbe85878851fa5cbcd7af19f2b27a3c6d31848d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-28 18:20:45 -0300

    test: Create auth middleware tests

--- tests/middlewares_spec.rs
+++ tests/middlewares_spec.rs
@@ -11,7 +11,8 @@ mod middlewares_specs {
         web::Path,
     };
     use navarro_blog_api::middlewares::{
-        jwt_token_middleware::jwt_token_middleware, uuid_path_middleware::uuid_path_middleware,
+        auth_middleware::auth_middleware, jwt_token_middleware::jwt_token_middleware,
+        uuid_path_middleware::uuid_path_middleware,
     };
 
     #[test]
@@ -20,7 +21,8 @@ mod middlewares_specs {
 
         let mut header_map_mock: HeaderMap = HeaderMap::new();
 
-        let jwt = JwtModels::access_jwt_model("123".to_string());
+        let id = uuid::Uuid::new_v4().to_string();
+        let jwt = JwtModels::access_jwt_model(id.clone());
 
         let authorization_value = format!("Bearer {}", jwt);
         let authorization_header_value = HeaderValue::from_str(&authorization_value).unwrap();
@@ -29,9 +31,9 @@ mod middlewares_specs {
             authorization_header_value,
         );
 
-        let resp = jwt_token_middleware(&header_map_mock).unwrap();
+        let token = jwt_token_middleware(&header_map_mock).unwrap();
 
-        assert_eq!(resp, ());
+        assert!(token.claims.sub.contains(&id));
     }
 
     #[test]
@@ -88,7 +90,7 @@ mod middlewares_specs {
 
         let uuid = uuid::Uuid::new_v4().to_string();
         let path = Path::from(uuid.clone());
-        let resp = uuid_path_middleware(path, "user_id").unwrap();
+        let resp = uuid_path_middleware(path.clone(), "user_id").unwrap();
 
         assert_eq!(resp, uuid);
     }
@@ -100,7 +102,9 @@ mod middlewares_specs {
         let uuid = String::from("Victor");
         let path_value = Path::from(uuid.clone());
         let path_name = "user_id";
-        let resp = uuid_path_middleware(path_value, path_name).err().unwrap();
+        let resp = uuid_path_middleware(path_value.clone(), path_name)
+            .err()
+            .unwrap();
         assert_eq!(resp.status(), 400);
 
         let bytes =
@@ -109,5 +113,95 @@ mod middlewares_specs {
         assert!(bytes.contains(path_name));
         assert!(bytes.contains("Por favor, envie um valor de UUID válido na URL da requisição."));
         assert!(bytes.contains("bad request"));
+    }
+
+    #[test]
+    async fn _auth() {
+        dotenv::dotenv().ok();
+
+        let id = uuid::Uuid::new_v4().to_string();
+        let jwt = JwtModels::access_jwt_model(id.clone());
+
+        let authorization_value = format!("Bearer {}", jwt);
+
+        let request = test::TestRequest::default()
+            .append_header(("Authorization", authorization_value))
+            .to_http_request();
+
+        let resp = auth_middleware(id, request, "user_id").await.unwrap();
+
+        assert_eq!(resp, ());
+    }
+
+    #[test]
+    async fn _auth_error_type_value() {
+        dotenv::dotenv().ok();
+
+        let id = String::from("Victor");
+        let jwt = JwtModels::access_jwt_model(id.clone());
+        let authorization_value = format!("Bearer {}", jwt);
+        let path_name = "user_id";
+
+        let request = test::TestRequest::default()
+            .append_header(("Authorization", authorization_value))
+            .to_http_request();
+
+        let resp = auth_middleware(id, request, path_name).await.err().unwrap();
+
+        assert_eq!(resp.status(), 400);
+
+        let bytes =
+            String::from_utf8(body::to_bytes(resp.into_body()).await.unwrap().to_vec()).unwrap();
+
+        assert!(bytes.contains(path_name));
+        assert!(bytes.contains("Por favor, envie um valor de UUID válido na URL da requisição."));
+        assert!(bytes.contains("bad request"));
+    }
+
+    #[test]
+    async fn _auth_error_refresh_token() {
+        dotenv::dotenv().ok();
+
+        let id = uuid::Uuid::new_v4().to_string();
+        let jwt = JwtModels::refresh_jwt_model("123".to_string());
+        let authorization_value = format!("Bearer {}", jwt);
+        let path_name = "user_id";
+
+        let request = test::TestRequest::default()
+            .append_header(("Authorization", authorization_value))
+            .to_http_request();
+
+        let resp = auth_middleware(id, request, path_name).await.err().unwrap();
+
+        assert_eq!(resp.status(), 401);
+
+        let bytes =
+            String::from_utf8(body::to_bytes(resp.into_body()).await.unwrap().to_vec()).unwrap();
+
+        assert!(bytes.contains("unauthorized"));
+        assert!(bytes.contains("bearer token"));
+    }
+
+    #[test]
+    async fn _auth_error_authorization_not_found() {
+        dotenv::dotenv().ok();
+
+        let id = uuid::Uuid::new_v4().to_string();
+        let authorization_value = "Bearer";
+        let path_name = "user_id";
+
+        let request = test::TestRequest::default()
+            .append_header(("content-type", authorization_value))
+            .to_http_request();
+
+        let resp = auth_middleware(id, request, path_name).await.err().unwrap();
+
+        assert_eq!(resp.status(), 400);
+
+        let bytes =
+            String::from_utf8(body::to_bytes(resp.into_body()).await.unwrap().to_vec()).unwrap();
+
+        assert!(bytes.contains("bad request"));
+        assert!(bytes.contains("O valor do cabeçalho 'Authorization' deve ser informado."));
     }
 }
