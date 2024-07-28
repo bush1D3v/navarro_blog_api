56465a966d80d0300f55cc71a66f5256a8bee4e0
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-28 18:27:59 -0300

    docs: Add annotation doc to the function

--- src/utils/error_construct.rs
+++ src/utils/error_construct.rs
@@ -1,8 +1,37 @@
 use crate::shared::structs::error_struct::{ErrorParams, ErrorStruct};
 use serde_json::json;
 
-// Erros do tipo validator::ValidationErros não precisam passar por aqui, pois esta é uma
-// função que visa simular um erro deste mesmo tipo, padronizando o retorno de erros e exceções.
+/// Construct an default error response.
+///
+/// This function constructs an error response with the provided `data`, `code`, `message`, `value`, `min`, and `max`.
+///
+/// # Parameters
+///
+/// - `data`: The input data that caused the error.
+/// - `code`: The text description of status code.
+/// - `message`: The message description of the error to be shown to the user.
+/// - `value`: The optional value of the input data.
+/// - `max`: The optional max length of the input data.
+/// - `min`: The optional min length of the input data.
+///
+/// # Returns
+///
+/// Returns a `serde_json::Value` with the default error response.
+///
+/// # Example
+///
+/// ```rust
+/// use navarro_blog_api::utils::error_construct::error_construct;
+///
+/// let error = error_construct(
+///     String::from("email"),
+///     String::from("conflict"),
+///     String::from("Este e-mail já está sendo utilizado por outro usuário."),
+///     Some(String::from("emailDeTeste@gmail.com")),
+///     Some(10),
+///     Some(127),
+/// );
+/// ```
 pub fn error_construct(
     data: String,
     code: String,
--- src/utils/query_constructor_executor.rs
+++ src/utils/query_constructor_executor.rs
@@ -5,6 +5,44 @@ use actix_web::HttpResponse;
 use sql_builder::SqlBuilder;
 use tokio_postgres::Row;
 
+/// Construct and execute the query.
+///
+/// This function constructs and executes the query, based on the provided `SqlBuilder` and `Pool`.
+///
+/// # Parameters
+///
+/// - `pg_pool`: A connection pool for the database.
+/// - `sql_builder`: An instance of the `SqlBuilder` struct.
+///
+/// # Returns
+///
+/// Returns a `Result` which, on success, contains the result of the query. On failure, returns an `HttpResponse` with the corresponding error.
+///
+/// # Errors
+///
+/// This function may return an error if:
+///
+/// - It is not possible to obtain a connection from the pool.
+/// - The transaction fails.
+/// - The conversion from `sql_builder::SqlBuilder` to `String` fails.
+/// - The query fails.
+/// - The commit fails.
+///
+/// # Example
+///
+/// ```rust
+/// use navarro_blog_api::utils::query_constructor_executor::query_constructor_executor;
+/// use sql_builder::SqlBuilder;
+/// use actix_web::{web::Data, HttpResponse};
+/// use deadpool_postgres::Pool;
+///
+/// pub async fn example(pg_pool: Data<Pool>, sql_builder: SqlBuilder) -> Result<Vec<postgres::Row>, HttpResponse> {
+///     match query_constructor_executor(pg_pool, sql_builder).await {
+///         Ok(x) => Ok(x),
+///         Err(e) => return Err(e),
+///     }
+/// }
+/// ```
 pub async fn query_constructor_executor(
     pg_pool: actix_web::web::Data<deadpool_postgres::Pool>,
     sql_builder: SqlBuilder,
@@ -21,7 +59,7 @@ pub async fn query_constructor_executor(
         Ok(x) => x,
         Err(e) => return Err(custom_error_to_io_error_kind(CustomError::AnyhowError(e))),
     };
-    let rows = match transaction.query(sql.as_str(), &[]).await {
+    let rows = match transaction.query(&sql, &[]).await {
         Ok(x) => x,
         Err(e) => return Err(custom_error_to_io_error_kind(CustomError::TokioPostgres(e))),
     };
