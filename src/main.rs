5459ad02334fefcce79cd216a25354c9687ada80
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-08-16 14:40:55 -0300

    docs: Creating annotation doc to main function

--- src/main.rs
+++ src/main.rs
@@ -17,23 +17,36 @@ mod modules;
 mod shared;
 mod utils;
 
+/// The main function that starts the server
+///
+/// It creates the database connection pool (Postgres and Redis), and starts the server with the configured routes.
+/// It also creates the queues for the user module to handle the asynchronous operations.
+///
+/// # Internal Variables
+///
+/// - `postgres_pool`: A connection pool for the postgres database.
+/// - `redis_pool`: A connection pool for the Redis database.
+/// - `insert_user_queue`: A queue for inserting a new user.
+/// - `delete_user_queue`: A queue for deleting an existing user.
+/// - `put_user_queue`: A queue for updating users for completely.
+///
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::init();
     dotenv::dotenv().ok();
 
     let redis_pool = Redis::pool().await;
-    let pool = Postgres::pool();
+    let postgres_pool = Postgres::pool();
 
-    let insert_pool_async = pool.clone();
+    let insert_pool_async = postgres_pool.clone();
     let insert_user_queue = Arc::new(InsertUserAppQueue::new());
     let insert_user_queue_async = insert_user_queue.clone();
 
-    let delete_pool_async = pool.clone();
+    let delete_pool_async = postgres_pool.clone();
     let delete_user_queue = Arc::new(DeleteUserAppQueue::new());
     let delete_user_queue_async = delete_user_queue.clone();
 
-    let put_pool_async = pool.clone();
+    let put_pool_async = postgres_pool.clone();
     let put_user_queue = Arc::new(PutUserAppQueue::new());
     let put_user_queue_async = put_user_queue.clone();
 
@@ -48,7 +61,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     HttpServer::new(move || {
         App::new()
             .wrap(cors())
-            .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(postgres_pool.clone()))
             .app_data(web::Data::new(redis_pool.clone()))
             .app_data(web::Data::new(insert_user_queue.clone()))
             .app_data(web::Data::new(delete_user_queue.clone()))
