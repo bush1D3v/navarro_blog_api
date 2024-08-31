4e600b3725345f0e32d05ad424bec00fcbf58689
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-08-30 21:31:22 -0300

    docs: Add annotation docs to user mocks models

--- tests/mocks/models/user.rs
+++ tests/mocks/models/user.rs
@@ -2,13 +2,36 @@ use bcrypt::hash;
 use navarro_blog_api::shared::structs::query_params::QueryParams;
 
 use crate::mocks::structs::user::{
-    MockDetailUserDTO, MockInsertUserDTO, MockLoginUserDTO, MockUserDTO,
+    MockDeleteUserDTO, MockDetailUserDTO, MockInsertUserDTO, MockLoginUserDTO, MockPutUserDTO,
+    MockUserDTO,
 };
 
-pub struct UserModels {}
+/// Models for Query Params
+///
+/// It contains the Query Params models default and to user entity.
+///
+/// # Functions
+///
+/// - `default_query_params_model()` - It creates a default Query Params model.
+/// - `limit_query_params_model()` - It creates a Query Params model with limit.
+/// - `offset_query_params_model()` - It creates a Query Params model with offset.
+/// - `order_by_query_params_model()` - It creates a Query Params model with order_by.
+/// - `order_direction_query_params_model()` - It creates a Query Params model with order_direction.
 pub struct QueryParamsModels {}
 
 impl QueryParamsModels {
+    /// Query Params Default Model
+    ///
+    /// It creates a default Query Params model.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::QueryParamsModels;
+    /// use navarro_blog_api::shared::structs::query_params::QueryParams;
+    ///
+    /// let query_params: QueryParams = QueryParamsModels::default_query_params_model();
+    /// ```
     pub fn default_query_params_model() -> QueryParams {
         QueryParams {
             limit: Some(20),
@@ -18,6 +41,18 @@ impl QueryParamsModels {
         }
     }
 
+    /// Query Params Limit Model
+    ///
+    /// It creates a Query Params model with limit.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::QueryParamsModels;
+    /// use navarro_blog_api::shared::structs::query_params::QueryParams;
+    ///
+    /// let limit_query_param: QueryParams = QueryParamsModels::limit_query_params_model(10);
+    /// ```
     pub fn limit_query_params_model(limit: i8) -> QueryParams {
         QueryParams {
             limit: Some(limit),
@@ -27,6 +62,18 @@ impl QueryParamsModels {
         }
     }
 
+    /// Query Params Offset Model
+    ///
+    /// It creates a Query Params model with offset.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::QueryParamsModels;
+    /// use navarro_blog_api::shared::structs::query_params::QueryParams;
+    ///
+    /// let offset_query_param: QueryParams = QueryParamsModels::offset_query_params_model(10);
+    /// ```
     pub fn offset_query_params_model(offset: i8) -> QueryParams {
         QueryParams {
             limit: None,
@@ -36,6 +83,18 @@ impl QueryParamsModels {
         }
     }
 
+    /// Query Params Order By Model
+    ///
+    /// It creates a Query Params model with order_by.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::QueryParamsModels;
+    /// use navarro_blog_api::shared::structs::query_params::QueryParams;
+    ///
+    /// let order_by_query_param: QueryParams = QueryParamsModels::order_by_query_params_model("created_at");
+    /// ```
     pub fn order_by_query_params_model(order_by: &str) -> QueryParams {
         QueryParams {
             limit: None,
@@ -45,6 +104,18 @@ impl QueryParamsModels {
         }
     }
 
+    /// Query Params Order Direction Model
+    ///
+    /// It creates a Query Params model with order_direction.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::QueryParamsModels;
+    /// use navarro_blog_api::shared::structs::query_params::QueryParams;
+    ///
+    /// let order_direction_query_param: QueryParams = QueryParamsModels::order_direction_query_params_model("desc");
+    /// ```
     pub fn order_direction_query_params_model(order_direction: &str) -> QueryParams {
         QueryParams {
             limit: None,
@@ -55,7 +126,33 @@ impl QueryParamsModels {
     }
 }
 
+/// Models for Users
+///
+/// It contains the Mocks UserDTO's models.
+///
+/// # Functions
+///
+/// - `complete_user_model()` - It creates a complete user model.
+/// - `complete_user_model_hashed()` - It creates a detail user model with hashed password.
+/// - `simple_user_model()` - It creates an insert user model.
+/// - `simple_user_model_hashed()` - It creates an simple user model with hashed password.
+/// - `login_user_model()` - It creates a login user model.
+/// - `detail_user_model()` - It creates a detail user model.
+pub struct UserModels {}
+
 impl UserModels {
+    /// Complete User Model
+    ///
+    /// It creates a complete user model.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::UserModels;
+    /// use navarro_blog_api::tests::mocks::structs::user::MockUserDTO;
+    ///
+    /// let user: MockUserDTO = UserModels::complete_user_model();
+    /// ```
     pub fn complete_user_model() -> MockUserDTO {
         MockUserDTO {
             id: uuid::Uuid::new_v4().to_string(),
@@ -67,6 +164,18 @@ impl UserModels {
         }
     }
 
+    /// Complete User Model Hashed
+    ///
+    /// It creates a detail user model with hashed password.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::UserModels;
+    /// use navarro_blog_api::tests::mocks::structs::user::MockUserDTO;
+    ///
+    /// let user: MockUserDTO = UserModels::complete_user_model_hashed();
+    /// ```
     pub fn complete_user_model_hashed() -> MockUserDTO {
         MockUserDTO {
             id: uuid::Uuid::new_v4().to_string(),
@@ -78,6 +187,18 @@ impl UserModels {
         }
     }
 
+    /// Simple User Model
+    ///
+    /// It creates an insert user model.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::UserModels;
+    /// use navarro_blog_api::tests::mocks::structs::user::MockInsertUserDTO;
+    ///
+    /// let user: MockInsertUserDTO = UserModels::simple_user_model();
+    /// ```
     pub fn simple_user_model() -> MockInsertUserDTO {
         MockInsertUserDTO {
             name: String::from("Victor Navarro"),
@@ -86,6 +207,18 @@ impl UserModels {
         }
     }
 
+    /// Simple User Model Hashed
+    ///
+    /// It creates an simple user model with hashed password.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::UserModels;
+    /// use navarro_blog_api::tests::mocks::structs::user::MockInsertUserDTO;
+    ///
+    /// let user: MockInsertUserDTO = UserModels::simple_user_model_hashed().into();
+    /// ```
     pub fn simple_user_model_hashed() -> MockInsertUserDTO {
         MockInsertUserDTO {
             name: String::from("Victor Navarro"),
@@ -94,6 +227,18 @@ impl UserModels {
         }
     }
 
+    /// Login User Model
+    ///
+    /// It creates a login user model.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::UserModels;
+    /// use navarro_blog_api::tests::mocks::structs::user::MockLoginUserDTO;
+    ///
+    /// let user: MockLoginUserDTO = UserModels::login_user_model();
+    /// ```
     pub fn login_user_model() -> MockLoginUserDTO {
         MockLoginUserDTO {
             email: String::from("bush1d3v@gmail.com"),
@@ -101,12 +246,64 @@ impl UserModels {
         }
     }
 
+    /// Detail User Model
+    ///
+    /// It creates a detail user model.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::UserModels;
+    /// use navarro_blog_api::tests::mocks::structs::user::MockDetailUserDTO;
+    ///
+    /// let user: MockDetailUserDTO = UserModels::detail_user_model();
+    /// ```
     pub fn detail_user_model() -> MockDetailUserDTO {
         MockDetailUserDTO {
             id: uuid::Uuid::new_v4().to_string(),
             email: String::from("bush1d3v@gmail.com"),
             name: String::from("Victor Navarro"),
             created_at: chrono::Utc::now().to_string(),
+            updated_at: None,
+        }
+    }
+
+    /// Delete User Model
+    ///
+    /// It creates a delete user model.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::UserModels;
+    /// use navarro_blog_api::tests::mocks::structs::user::MockDeleteUserDTO;
+    ///
+    /// let user: MockDeleteUserDTO = UserModels::delete_user_model();
+    /// ```
+    pub fn delete_user_model() -> MockDeleteUserDTO {
+        MockDeleteUserDTO {
+            password: String::from("12345678%"),
+        }
+    }
+
+    /// Put User Model
+    ///
+    /// It creates a put user model.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::user::UserModels;
+    /// use navarro_blog_api::tests::mocks::structs::user::MockPutUserDTO;
+    ///
+    /// let user: MockPutUserDTO = UserModels::put_user_model();
+    /// ```
+    pub fn put_user_model() -> MockPutUserDTO {
+        MockPutUserDTO {
+            email: String::from("bush1d3v@gmail.com"),
+            password: String::from("12345678%"),
+            new_email: String::from("bush1d3v2@gmail.com"),
+            new_password: String::from("123456789%"),
         }
     }
 }
