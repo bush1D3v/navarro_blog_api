24a794b269f74cdc3033367516bac95f23378430
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-08-30 21:16:00 -0300

    docs: Add annotation docs to dto

--- tests/mocks/structs/user.rs
+++ tests/mocks/structs/user.rs
@@ -1,6 +1,18 @@
 use navarro_blog_api::modules::user::user_dtos::{InsertUserDTO, LoginUserDTO, PutUserDTO};
 use serde::{Deserialize, Serialize};
 
+/// Mock User DTO (UserDTO)
+///
+/// It's used to mock the UserDTO struct.
+///
+/// # Examples
+///
+/// ```rust
+/// use navarro_blog_api::mocks::models::user::UserModels;
+/// use navarro_blog_api::mocks::structs::user::MockUserDTO;
+///
+/// let user: MockUserDTO = UserModels::complete_user_model();
+/// ```
 #[derive(Serialize, Deserialize, Clone)]
 pub struct MockUserDTO {
     pub id: String,
@@ -11,6 +23,18 @@ pub struct MockUserDTO {
     pub updated_at: Option<String>,
 }
 
+/// Mock Insert User DTO (InsertUserDTO)
+///
+/// It's used to mock the InsertUserDTO struct.
+///
+/// # Examples
+///
+/// ```rust
+/// use navarro_blog_api::mocks::models::user::UserModels;
+/// use navarro_blog_api::mocks::structs::user::MockInsertUserDTO;
+///
+/// let user: MockInsertUserDTO = UserModels::simple_user_model();
+/// ```
 #[derive(Serialize, Deserialize, Clone)]
 pub struct MockInsertUserDTO {
     pub name: String,
@@ -28,6 +52,18 @@ impl Into<InsertUserDTO> for MockInsertUserDTO {
     }
 }
 
+/// Mock Login User DTO (LoginUserDTO)
+///
+/// It's used to mock the LoginUserDTO struct.
+///
+/// # Examples
+///
+/// ```rust
+/// use navarro_blog_api::mocks::models::user::UserModels;
+/// use navarro_blog_api::mocks::structs::user::MockLoginUserDTO;
+///
+/// let user: MockLoginUserDTO = UserModels::login_user_model();
+/// ```
 #[derive(Serialize, Deserialize, Clone)]
 pub struct MockLoginUserDTO {
     pub email: String,
@@ -43,19 +79,56 @@ impl Into<LoginUserDTO> for MockLoginUserDTO {
     }
 }
 
+/// Mock Detail User DTO (DetailUserDTO)
+///
+/// It's used to mock the DetailUserDTO struct.
+///
+/// # Examples
+///
+/// ```rust
+/// use navarro_blog_api::mocks::models::user::UserModels;
+/// use navarro_blog_api::mocks::structs::user::MockDetailUserDTO;
+///
+/// let user: MockDetailUserDTO = UserModels::detail_user_model();
+/// ```
 #[derive(Serialize, Deserialize, Clone)]
 pub struct MockDetailUserDTO {
     pub id: String,
     pub name: String,
     pub email: String,
     pub created_at: String,
+    pub updated_at: Option<String>,
 }
 
+/// Mock Delete User DTO (DeleteUserDTO)
+///
+/// It's used to mock the DeleteUserDTO struct.
+///
+/// # Examples
+///
+/// ```rust
+/// use navarro_blog_api::mocks::models::user::UserModels;
+/// use navarro_blog_api::mocks::structs::user::MockDeleteUserDTO;
+///
+/// let user: MockDeleteUserDTO = UserModels::delete_user_model();
+/// ```
 #[derive(Serialize, Deserialize, Clone)]
 pub struct MockDeleteUserDTO {
     pub password: String,
 }
 
+/// Mock Put User DTO (PutUserDTO)
+///
+/// It's used to mock the PutUserDTO struct.
+///
+/// # Examples
+///
+/// ```rust
+/// use navarro_blog_api::mocks::models::user::UserModels;
+/// use navarro_blog_api::mocks::structs::user::MockPutUserDTO;
+///
+/// let user: MockPutUserDTO = UserModels::put_user_model();
+/// ```
 #[derive(Serialize, Deserialize, Clone)]
 pub struct MockPutUserDTO {
     pub password: String,
