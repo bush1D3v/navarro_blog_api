d86411aaec1d670440c30dd6b4a56ed728ba0615
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-08-30 21:56:55 -0300

    docs: Create validate_body_error annotation docs

--- src/utils/validate_body_error.rs
+++ src/utils/validate_body_error.rs
@@ -1,9 +1,10 @@
 use actix_web::HttpResponse;
 use std::collections::HashMap;
+use validator::ValidationError;
 
 /// Validate body error.
 ///
-/// Validate body error,differentiating the error between 400 and 422.
+/// Validate body error, differentiating the error between 400 and 422.
 ///
 /// # Parameters
 ///
@@ -16,37 +17,22 @@ use std::collections::HashMap;
 /// # Example
 ///
 /// ```rust
+/// use navarro_blog_api::modules::user::user_dtos::InsertUserDTO;
+/// use navarro_blog_api::utils::validate_body_error::validate_body_error;
+/// use actix_web::{web, HttpResponse, Responder};
+/// use validator::Validate;
+///
 /// async fn insert_user(
 ///     body: web::Json<InsertUserDTO>,
-///     queue: web::Data<Arc<InsertUserAppQueue>>,
-///     redis_pool: web::Data<deadpool_redis::Pool>,
-///     postgres_pool: web::Data<deadpool_postgres::Pool>,
 /// ) -> impl Responder {
 ///     match body.validate() {
-///         Ok(_) => (),
-///         Err(e) => return validate_body_error(e.field_error()),
-///     };
-///     let redis_user = match Redis::get(&redis_pool, &body.email.clone()).await {
-///         Ok(redis_user) => redis_user,
-///         Err(_) => String::from(""),
-///     };
-///     match insert_user_service(queue.clone(), postgres_pool, body, redis_user).await {
-///         Ok(resp) => match UserSerdes::serde_json_to_string(&resp) {
-///             Ok(redis_user) => {
-///                 let _ = Redis::set(&redis_pool, &resp.id, &redis_user).await;
-///                 let _ = Redis::set(&redis_pool, &resp.email, &redis_user).await;
-///                 HttpResponse::Created()
-///                     .append_header(("Location", format!("/user/{}", resp.id)))
-///                     .finish()
-///             }
-///             Err(e) => e,
-///         },
-///             Err(e) => e,
+///         Ok(_) => HttpResponse::Created().json(body),
+///         Err(e) => validate_body_error(e.field_errors()),
 ///     }
 /// }
 /// ```
 pub fn validate_body_error(
-    field_errors: HashMap<&'static str, &Vec<validator::ValidationError>>,
+    field_errors: HashMap<&'static str, &Vec<ValidationError>>,
 ) -> HttpResponse {
     let mut errors = Vec::new();
 
