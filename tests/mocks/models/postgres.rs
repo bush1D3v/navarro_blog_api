61520a229d8853fb4f1a926ac7634faf8c0ca942
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-08-30 21:31:02 -0300

    docs: Add annotation docs to postgres mocks models

--- tests/mocks/models/postgres.rs
+++ tests/mocks/models/postgres.rs
@@ -2,9 +2,32 @@ use deadpool_postgres::{Config, Pool, PoolConfig, Runtime};
 use std::env;
 use tokio_postgres::NoTls;
 
+/// Models for Postgres
+///
+/// It contains the Postgres Pool Connection models.
+///
+/// # Functions
+///
+/// - `postgres_success()` - It creates a Success Postgres Pool Connection.
+/// - `postgres_error()` - It creates a Error Postgres Pool Connection.
 pub struct PostgresModels {}
 
 impl PostgresModels {
+    /// Postgres Success model
+    ///
+    /// It creates a Success Postgres Pool Connection.
+    ///
+    /// # Recommended Use
+    ///
+    /// In tests, these model is used to guarantee that Postgres is available.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::postgres::PostgresModels;
+    ///
+    /// let pool = PostgresModels::postgres_success();
+    /// ```
     pub fn postgres_success() -> Pool {
         let mut cfg: Config = Config::new();
         cfg.host = Some(env::var("DB_HOST").unwrap());
@@ -18,6 +41,21 @@ impl PostgresModels {
         cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap()
     }
 
+    /// Postgres Error model
+    ///
+    /// It creates a Error Postgres Pool Connection.
+    ///
+    /// # Recommended Use
+    ///
+    /// In tests, these model is used to throw a postgres connection error.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use navarro_blog_api::mocks::models::postgres::PostgresModels;
+    ///
+    /// let pool = PostgresModels::postgres_error();
+    /// ```
     pub fn postgres_error() -> Pool {
         let mut cfg: Config = Config::new();
         cfg.host = Some(env::var("DB_HOST").unwrap());
