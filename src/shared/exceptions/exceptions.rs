6c43d5252aa0386f05b489ac662ef5a4cc19b6ff
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-07 17:54:16 -0300

    refactor: Remove unneeded return statement

--- src/shared/exceptions/exceptions.rs
+++ src/shared/exceptions/exceptions.rs
@@ -6,31 +6,31 @@ pub struct Exceptions;
 impl Exceptions {
     /// Error 400
     pub fn bad_request(data: String, message: String) -> HttpResponse {
-        return HttpResponse::BadRequest().json(error_construct(
+        HttpResponse::BadRequest().json(error_construct(
             data,
             String::from("bad request"),
             message,
             None,
             None,
             None,
-        ));
+        ))
     }
 
     /// Error 401
     pub fn unauthorized(data: String, message: String, value: Option<String>) -> HttpResponse {
-        return HttpResponse::Unauthorized().json(error_construct(
+        HttpResponse::Unauthorized().json(error_construct(
             data,
             String::from("unauthorized"),
             message,
             value,
             None,
             None,
-        ));
+        ))
     }
 
     /// Error 403
     pub fn forbidden(value: String) -> HttpResponse {
-        return HttpResponse::Forbidden().json(error_construct(
+        HttpResponse::Forbidden().json(error_construct(
                 String::from("user"),
                 String::from("forbidden"),
                 String::from(
@@ -39,31 +39,31 @@ impl Exceptions {
                 Some(value),
                 None,
                 None,
-            ));
+            ))
     }
 
     /// Error 404
     pub fn not_found(data: String, message: String, value: Option<String>) -> HttpResponse {
-        return HttpResponse::NotFound().json(error_construct(
+        HttpResponse::NotFound().json(error_construct(
             data,
             String::from("not found"),
             message,
             value,
             None,
             None,
-        ));
+        ))
     }
 
     /// Error 409
     pub fn conflict(value: String) -> HttpResponse {
-        return HttpResponse::Conflict().json(error_construct(
+        HttpResponse::Conflict().json(error_construct(
             String::from("email"),
             String::from("conflict"),
             String::from("Este e-mail já está sendo utilizado por outro usuário."),
             Some(value),
             None,
             None,
-        ));
+        ))
     }
 
     ///Error 422
@@ -72,25 +72,25 @@ impl Exceptions {
         message: String,
         value: Option<String>,
     ) -> HttpResponse {
-        return HttpResponse::UnprocessableEntity().json(error_construct(
+        HttpResponse::UnprocessableEntity().json(error_construct(
             data,
             String::from("unprocessable entity"),
             message,
             value,
             None,
             None,
-        ));
+        ))
     }
 
     /// Error 500
     pub fn internal_server_error(data: String, message: String) -> HttpResponse {
-        return HttpResponse::InternalServerError().json(error_construct(
+        HttpResponse::InternalServerError().json(error_construct(
             data,
             String::from("internal server error"),
             message,
             None,
             None,
             None,
-        ));
+        ))
     }
 }
