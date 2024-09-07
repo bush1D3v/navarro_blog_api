e3ebbaba8460268de6c5bb36020e188e29ac78bf
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-07 20:00:56 -0300

    docs: Add annotation docs to jwt_claims

--- src/shared/structs/jwt_claims.rs
+++ src/shared/structs/jwt_claims.rs
@@ -1,5 +1,16 @@
 use serde::{Deserialize, Serialize};
 
+/// # JWT Claims
+///
+/// ## Fields
+///
+/// * `sub` - Subject
+/// * `role` - Role
+/// * `exp` - Expiration
+///
+/// ## Purpose
+///
+/// Create a default Struct for JWT Claims.
 #[derive(Deserialize, Serialize)]
 pub struct Claims {
     pub sub: String,
