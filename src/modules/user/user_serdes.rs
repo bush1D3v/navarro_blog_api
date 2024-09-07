1f3224f77266c7c992b30b41e00177785c7c2c3c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-07 20:00:32 -0300

    docs: Add annotation docs to user_serdes

--- src/modules/user/user_serdes.rs
+++ src/modules/user/user_serdes.rs
@@ -2,9 +2,31 @@ use super::user_dtos::UserDTO;
 use crate::shared::exceptions::exception::Exception;
 use actix_web::HttpResponse;
 
+/// # User Serdes Module
+///
+/// This module contains all the serdes for the user endpoints.
+///
+/// ## Usage
+///
+/// This module is used to define the serdes for the user endpoints.
+///
+/// ## Returns
+///
+/// This function returns a `UserSerdes` instance, which is used by the web server to serialize and deserialize the user DTO.
 pub struct UserSerdes;
 
 impl UserSerdes {
+    /// Serialize User DTO
+    ///
+    /// This function serializes the user DTO to a string.
+    ///
+    /// # Parameters
+    ///
+    /// - `user`: The user DTO to be serialized.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` which, on success, contains the serialized user DTO. On failure, returns an `HttpResponse` with the corresponding error.
     pub fn serde_json_to_string(user: &UserDTO) -> Result<String, HttpResponse> {
         match serde_json::to_string(user) {
             Ok(x) => Ok(x),
@@ -15,6 +37,17 @@ impl UserSerdes {
         }
     }
 
+    /// Deserialize User DTO
+    ///
+    /// This function deserializes the user DTO from a string.
+    ///
+    /// # Parameters
+    ///
+    /// - `user`: The user DTO to be deserialized.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` which, on success, contains the deserialized user DTO. On failure, returns an `HttpResponse` with the corresponding error.
     pub fn serde_string_to_json(user: &str) -> Result<UserDTO, HttpResponse> {
         match serde_json::from_str(user) {
             Ok(x) => Ok(x),
