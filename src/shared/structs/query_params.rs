0cb9076f6a16704ad6b219269813a5f08c2cdb46
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-07 20:01:04 -0300

    docs: Add annotation docs to query_params

--- src/shared/structs/query_params.rs
+++ src/shared/structs/query_params.rs
@@ -1,6 +1,18 @@
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
+/// # Query Params
+///
+/// ## Fields
+///
+/// * `limit` - Limit of query
+/// * `offset` - Offset of query
+/// * `order_by` - Order by
+/// * `order_direction` - Order direction
+///
+/// ## Purpose
+///
+/// Create a default Struct for query params.
 #[derive(ToSchema, Serialize, Deserialize)]
 pub struct QueryParams {
     pub limit: Option<i8>,
