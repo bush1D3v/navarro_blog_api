66661a921cc35bfbb8adb1e71cd8adca6d65b6e5
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-09-07 20:01:19 -0300

    docs: Add annotation docs to jwt_treated

--- src/shared/treaties/jwt_treated.rs
+++ src/shared/treaties/jwt_treated.rs
@@ -3,9 +3,34 @@ use actix_web::HttpResponse;
 use jsonwebtoken::TokenData;
 use std::env;
 
+/// # JWT
+///
+/// ## Purpose
+///
+/// Treat JWT token models, decode and encode tokens.
+///
+/// ## Functions
+///
+/// - `refresh_token_constructor()` - It creates a refresh token
+/// - `access_token_constructor()` - It creates a access token
+/// - `access_token_decode()` - It decodes an access token
+/// - `refresh_token_decode()` - It decodes a refresh token
 pub struct Jwt {}
 
 impl Jwt {
+    /// # Refresh Token Constructor
+    ///
+    /// ## Arguments
+    ///
+    /// * `user_id` - String
+    ///
+    /// ## Returns
+    ///
+    /// Refresh token
+    ///
+    /// ## Errors
+    ///
+    /// - Internal Server Error - If it fails to create the refresh token
     pub fn refresh_token_constructor(user_id: String) -> Result<String, HttpResponse> {
         let claims = Claims {
             sub: user_id,
@@ -26,6 +51,19 @@ impl Jwt {
         }
     }
 
+    /// # Access Token Constructor
+    ///
+    /// ## Arguments
+    ///
+    /// * `user_id` - String
+    ///
+    /// ## Returns
+    ///
+    /// Access token
+    ///
+    /// ## Errors
+    ///
+    /// - Internal Server Error - If it fails to create the access token
     pub fn access_token_constructor(user_id: String) -> Result<String, HttpResponse> {
         let claims = Claims {
             sub: user_id,
@@ -46,6 +84,19 @@ impl Jwt {
         }
     }
 
+    /// # Access Token Decode
+    ///
+    /// ## Arguments
+    ///
+    /// * `token` - String
+    ///
+    /// ## Returns
+    ///
+    /// Access token data
+    ///
+    /// ## Errors
+    ///
+    /// - Unauthorized - If it fails to decode the access token
     pub fn access_token_decode(token: &str) -> Result<TokenData<Claims>, HttpResponse> {
         match jsonwebtoken::decode::<Claims>(
             token,
@@ -63,6 +114,19 @@ impl Jwt {
         }
     }
 
+    /// # Refresh Token Decode
+    ///
+    /// ## Arguments
+    ///
+    /// * `token` - String
+    ///
+    /// ## Returns
+    ///
+    /// Refresh token data
+    ///
+    /// ## Errors
+    ///
+    /// - Unauthorized - If it fails to decode the refresh token
     pub fn _refresh_token_decode(token: &str) -> Result<TokenData<Claims>, HttpResponse> {
         match jsonwebtoken::decode::<Claims>(
             token,
